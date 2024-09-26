679024471b8e024601a404a49d24c3150329f042
elmin007 <imanverdiyevemin414@gmail.com>
2024-09-27 01:52:17 +0400

    Added service for example

---
+++ src/Infrastructure/EComPayApp.Infrastructure/Services/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using EComPayApp.Application.DTOs.ProductDtos;
+using EComPayApp.Application.Interfaces.Services;
+using EComPayApp.Application.Interfaces.UoW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComPayApp.Infrastructure.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public ProductService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IQueryable<ProductListDto>> GetAllProductsAsync()
+        {
+            var products = await _unitOfWork.Products.GetAll(); // GetAllAsync metodu IRepository-də olmalıdır.
+            var productListDtos = _mapper.ProjectTo<ProductListDto>(products);
+            return productListDtos;
+        }
+
+        public async Task<ProductDto> GetProductByIdAsync(Guid productId)
+        {
+            var product = await _unitOfWork.Products.GetByIdAsync(productId);
+            return _mapper.Map<ProductDto>(product);
+        }
+
+        public async Task<bool> CreateProductAsync(ProductDto createProductDto)
+        {
+            var product = _mapper.Map<Product>(createProductDto);
+            return await _unitOfWork.Products.AddAsync(product);
+        }
+
+        public async Task<bool> UpdateProductAsync(ProductDto updateProductDto)
+        {
+            var product = _mapper.Map<Product>(updateProductDto);
+            return _unitOfWork.Products.Update(product);
+        }
+
+        public async Task<bool> DeleteProductAsync(Guid productId)
+        {
+            return await _unitOfWork.Products.Remove(productId);
+        }
+    }
+}
